28b7dd4b6a8eb60f996f8db24e479981b68fa39e
Neha <neha.yapatla@gmail.com>
2026-05-25 02:02:18 +0530

    fix cors for vercel

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ async def startup():
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173","https://bakes-by-neha.vercel.app","https://bakesbyneha.onrender.com","https://your-vercel-app.vercel.app",        "http://localhost:5174",
+        "http://localhost:5173","https://bakes-by-neha.vercel.app","https://bakesbyneha.onrender.com","https://your-vercel-app.vercel.app","http://localhost:5174","https://bakes-by-neha-9fl1f5wv4-neha1393s-projects.vercel.app",
         "http://localhost:5175",
         "http://localhost:5176",
     ],
